1db1eed88277ba37a1efbafe672f539852bbb66b
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-08 20:31:58 -0500

    Collection view implemented

---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Connie Liu on 7/8/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
